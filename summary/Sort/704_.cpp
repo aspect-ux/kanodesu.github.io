cc7c78c05a69c309d6f9537aca01dc3f3291dbc1
aspect-ux <2228217372@qq.com>
2022-06-14 18:04:27 +0800

    1

--- summary/Sort/704_.cpp
+++ summary/Sort/704_.cpp
@@ -18,8 +18,31 @@ public:
         return -1;
     }
 };
+void insert_sort(vector<int> nums){
+    int j = 0,target;
+    for(int i = 0;i < nums.size(); i++){
+        target = nums[i];
+        j = i;
+        for(;j <= i; j++){
+            if(target > nums[j + 1]){
+                nums[j] = nums[j + 1];
+            }
+            else break;
+        }
+        if(j > i){
+            nums[j + 1] = target;
+        }
+   }
+}
+void Print(vector<int> nums){
+    for(int i = 0;i < nums.size(); i++){
+        cout<<nums[i]<<endl;
+    }
+}
 int main(){
-
+    vector<int> nums={1,2,4,2,1,4,2,8,3,7,2};
+    insert_sort(nums);
+    Print(nums);
     system("pause:");
     return 0;
 }
