9ae272c058bd574d47f61b04fbfe2f08e51dbe77
aspect-ux <2228217372@qq.com>
2022-12-30 11:46:44 +0800

    lc

--- Algorithm/Cplusplus/Cplusplus/SwordToOffer/068.cpp
+++ Algorithm/Cplusplus/Cplusplus/SwordToOffer/068.cpp
@@ -4,6 +4,15 @@ using namespace std;
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
+        int low = 0,high = nums.size() - 1,mid = (high - low) / 2 + low;
+        while (low <= high) {
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid - 1;
+            else {
+                low = mid + 1;
+            }
+            mid = (high - low) / 2 + low;
+        }
+        return mid;
     }
 };
